0a12fc76e2eab7adf3e9308501f5a2ba8e4e246f
Ankush Gawande <ank.gawande@gmail.com>
2023-04-20 15:21:41 +0530

    Ankush - Cache categories on Home page for 30 seconds

--- AzureRedisCache/Controllers/HomeController.cs
+++ AzureRedisCache/Controllers/HomeController.cs
@@ -1,5 +1,8 @@
-﻿using AzureRedisCache.Models;
+﻿using AzureRedisCache.Data;
+using AzureRedisCache.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
 using System.Diagnostics;
 
 namespace AzureRedisCache.Controllers
@@ -7,15 +10,41 @@ namespace AzureRedisCache.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
+        private readonly ApplicationDbContext _dbContext;
+        private readonly IDistributedCache _distributedCache;
+        public HomeController(ILogger<HomeController> logger,
+            ApplicationDbContext dbContext,
+            IDistributedCache distributedCache)
         {
             _logger = logger;
+            _dbContext = dbContext;
+            _distributedCache = distributedCache;
         }
 
         public IActionResult Index()
         {
-            return View();
+            //// Removed item from Cache
+            //_distributedCache.Remove("CategoryList");
+
+            List<Category> categoryList = new();
+            var cachedCategories = _distributedCache.GetString("CategoryList");
+
+            if (!string.IsNullOrEmpty(cachedCategories))
+            {
+                categoryList = JsonConvert.DeserializeObject<List<Category>>(cachedCategories);
+            }
+            else
+            {
+                categoryList = _dbContext.Category.ToList();
+
+                // Cache expiration after 30 seconds
+                DistributedCacheEntryOptions options = new();
+                options.SetAbsoluteExpiration(new TimeSpan(0, 0, 30));
+
+                // Set cache
+                _distributedCache.SetString("CategoryList", JsonConvert.SerializeObject(categoryList), options);
+            }
+            return View(categoryList);
         }
 
         public IActionResult Privacy()
--- AzureRedisCache/Models/ErrorViewModel.cs
+++ AzureRedisCache/Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace AzureRedisCache.Models
 {
     public class ErrorViewModel
     {
-        public string? RequestId { get; set; }
+        public string RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
